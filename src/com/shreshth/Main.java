11e2b29f03725a75d43b01c93a1601df0e3b8a4e
shresth <shresthsingh1003@gmail.com>
2023-06-04 23:45:50 +0530

    done

---
+++ src/com/shreshth/Main.java
@@ -0,0 +1,15 @@
+package com.shreshth;
+
+public class Main {
+
+	
+
+	public static void main(String[] args) {
+	Account ShreshthAccount = new Account("123456789",0.00,"Shreshth" ,"shreshth@gmail","121212");
+     
+	
+	ShreshthAccount.DepositMoney(10000);
+	ShreshthAccount.withDrow(500);
+	}
+
+}
